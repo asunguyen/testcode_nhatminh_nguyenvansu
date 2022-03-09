8be41249144ffcfc1bfc821944f77d31b3481cb3
asunguyen <cntt510@gmail.com>
2022-03-09 14:56:47 +0700

    update detail

---
+++ src/components/detail-trade/detail-trade.js
@@ -0,0 +1,12 @@
+export default {
+    data: function() {
+        return {
+
+        }
+    },
+    methods: {
+        close: function() {
+            document.getElementById("detail-trade").style.display = "none"
+        }
+    }
+}
--- src/views/dashboard/dashboard.js
+++ src/views/dashboard/dashboard.js
@@ -1,9 +1,19 @@
 import {data} from "./data.js";
-
+import detailTrade from "@/components/detail-trade/detail-trade.vue";
 export default{
+    components: {
+        detailTrade
+    },
     data: function() {
         return {
-            datav: data
+            datav: data,
+            itemSl: {}
+        }
+    },
+    methods: {
+        viewDetail: function(item) {
+            this.itemSl = item;
+            document.getElementById("detail-trade").style.display = "block";
         }
     }
 }
